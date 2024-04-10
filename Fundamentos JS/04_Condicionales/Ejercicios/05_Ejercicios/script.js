4f110fd5d61b2e79a0d0c3b44ab9eb8255738440
VacSSJ <vicentearaya@liceovvh.cl>
2024-04-10 11:46:47 -0400

    ok

--- Fundamentos JS/04_Condicionales/Ejercicios/05_Ejercicios/script.js
+++ Fundamentos JS/04_Condicionales/Ejercicios/05_Ejercicios/script.js
@@ -1,13 +1,17 @@
 function puntaje(){
     let puntuacion = parseInt(prompt(`Ingrese su puntuacion`))
+    let resultado = ""
     if(puntuacion >= 90){
         resultado = (`<h2>${puntuacion} WoW exelente puntaje</h2>`)
     }
-    else if(puntuacion >= 70 , puntuacion >= 89){
+    else if(puntuacion >= 70 && puntuacion <= 89){
         resultado = (`<h2>${puntuacion} buen trabajo, sigue mejorando</h2>`)
     }
     else if(puntuacion < 70){
         resultado = (`<h2>${puntuacion} Necesitas mejorar</h2`)
+    }
+    else{
+        resultado = (`Ingrese el valor valido`)
     }
     document.getElementById(`lista`).innerHTML = resultado
 }
