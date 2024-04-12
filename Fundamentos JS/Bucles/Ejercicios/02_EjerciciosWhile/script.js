a7a76c86ee026eb6117986f572abdccc62aba0d3
VacSSJ <vicentearaya@liceovvh.cl>
2024-04-12 10:41:02 -0400

    ok

--- Fundamentos JS/Bucles/Ejercicios/02_EjerciciosWhile/script.js
+++ Fundamentos JS/Bucles/Ejercicios/02_EjerciciosWhile/script.js
@@ -1,10 +1,15 @@
 function Suma(){
-    let contador = 0
+    let contador = 1
+    let suma = 0
+    let sumaStr = ""
     let Array = []
-    while(contador <= 10){
-        Array.push(contador)
-        Array.push("+")
-        contador += 1 
+    while(contador < 10){
+        suma += contador
+        sumaStr += contador + "+"
+        Array.push(suma)
+        contador++
     }
-    document.getElementById("lista").innerHTML = `<h2>Numeros del 1 al 10: <br>${Array}</h2>`
+    suma += contador
+    sumaStr += contador + " = " +  suma
+    document.getElementById("lista").innerHTML = `<h2>Numeros del 1 al 10: <br>${sumaStr}</h2>`
 }
